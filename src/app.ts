80dc9f7af7b56865f64daff5d640d487cda45d40
Scott O'Malley <scottyomalley@gmail.com>
2016-11-21 17:08:26 +0000

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
+export class App {
+  router : Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Brian Boru Fencing Club';
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/home' }
+    ]);
+  }
+}
