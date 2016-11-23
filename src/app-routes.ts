cdcfa867b6c623978c7519356482a9673a8e0e1d
Scott O'Malley <scottyomalley@gmail.com>
2016-11-23 18:45:55 +0000

    Added new route for courses and lessons

--- src/app-routes.ts
+++ src/app-routes.ts
@@ -2,7 +2,7 @@ import {RouteConfig} from 'aurelia-router';
 
 export const routes : RouteConfig[] = [
   { route: ['', 'home'],    name: 'home',   moduleId: 'home/home', nav: false},
-  { route: 'home#courses-and-lessons', name: 'courses', moduleId: 'home/home', nav: true, title: 'Courses and lessons'},
-  { route: 'home#where-and-when', name: 'location', moduleId: 'home/home', nav: true, title: 'Where and when'},
-  { route: 'home#contact-us', name: 'contact-us', moduleId: 'home/home', nav: true, title: 'Contact Us'}
+  { route: 'courses-and-lessons', name: 'courses', moduleId: 'courses/courses', nav: true, title: 'Courses and lessons'},
+  { route: 'where-and-when', name: 'location', moduleId: 'home/home', nav: true, title: 'Where and when'},
+  { route: 'contact-us', name: 'contact-us', moduleId: 'home/home', nav: true, title: 'Contact Us'}
 ];
