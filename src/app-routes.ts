14186f429bc531bd9083e639c435172b55994406
Scott O'Malley <scottyomalley@gmail.com>
2016-11-24 23:50:28 +0000

    Added club time table

--- src/app-routes.ts
+++ src/app-routes.ts
@@ -3,6 +3,6 @@ import {RouteConfig} from 'aurelia-router';
 export const routes : RouteConfig[] = [
   { route: ['', 'home'],    name: 'home',   moduleId: 'home/home', nav: false},
   { route: 'courses-and-lessons', name: 'classes', moduleId: 'classes/classes', nav: true, title: 'Courses and lessons'},
-  { route: 'where-and-when', name: 'location', moduleId: 'home/home', nav: true, title: 'Where and when'},
+  { route: 'where-and-when', name: 'location', moduleId: 'where-and-when/where-and-when', nav: true, title: 'Where and when'},
   { route: 'contact-us', name: 'contact-us', moduleId: 'home/home', nav: true, title: 'Contact Us'}
 ];
