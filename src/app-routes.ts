f5004ed81dbfa9a7bd3643e3af6095f6dc90cfed
Scott O'Malley <scottyomalley@gmail.com>
2016-11-24 13:02:11 +0000

    Renamed state, added courses

--- src/app-routes.ts
+++ src/app-routes.ts
@@ -2,7 +2,7 @@ import {RouteConfig} from 'aurelia-router';
 
 export const routes : RouteConfig[] = [
   { route: ['', 'home'],    name: 'home',   moduleId: 'home/home', nav: false},
-  { route: 'courses-and-lessons', name: 'courses', moduleId: 'courses/courses', nav: true, title: 'Courses and lessons'},
+  { route: 'courses-and-lessons', name: 'classes', moduleId: 'classes/classes', nav: true, title: 'Courses and lessons'},
   { route: 'where-and-when', name: 'location', moduleId: 'home/home', nav: true, title: 'Where and when'},
   { route: 'contact-us', name: 'contact-us', moduleId: 'home/home', nav: true, title: 'Contact Us'}
 ];
