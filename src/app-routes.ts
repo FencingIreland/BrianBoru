7082df1541a70960d27b5e8c92db1dc593ac3c6f
Scott O'Malley <scottyomalley@gmail.com>
2016-11-25 02:52:53 +0000

    text tweaks and UI/UX cleanup

--- src/app-routes.ts
+++ src/app-routes.ts
@@ -1,7 +1,8 @@
 import {RouteConfig} from 'aurelia-router';
 
 export const routes : RouteConfig[] = [
-  { route: ['', 'home'],    name: 'home',   moduleId: 'home/home', nav: false},
+  { route: ['', 'home'],    name: 'home',   moduleId: 'home/home', nav: false, title: 'Home'},
+  { route: 'membership', name: 'membership', moduleId: 'home/home', nav: true, title: 'Membership'},
   { route: 'courses-and-lessons', name: 'classes', moduleId: 'classes/classes', nav: true, title: 'Courses and lessons'},
   { route: 'where-and-when', name: 'location', moduleId: 'where-and-when/where-and-when', nav: true, title: 'Where and when'},
   { route: 'contact-us', name: 'contact-us', moduleId: 'home/home', nav: true, title: 'Contact Us'}
